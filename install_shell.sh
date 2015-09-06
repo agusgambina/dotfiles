56ab4e02e90dddd71c6d4e9daa24dc361f4228d5
Agustin Gambina <agusgambina+github@gmail.com>
2015-09-06 11:11:47 -0300

    add configuration for vimrc

--- install_shell.sh
+++ install_shell.sh
@@ -15,7 +15,8 @@ ln -sfv "$DOTFILES_DIR/system/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
-
+ln -sfv "$DOTFILES_DIR/vim/.nvimrc" ~
+ln -s ~/.vim ~/.nvim ~
 
 # if [ "$(uname)" == "Darwin" ]; then
 #     ln -sfv "$DOTFILES_DIR/etc/mackup/.mackup.cfg" ~
